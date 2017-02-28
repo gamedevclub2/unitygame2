f84a9d218569c55f509741bf842395e37b7bf113
RJHS\BeattyTh <BeattyTh@Student.RegisJesuit.com>
2017-02-28 13:15:20 -0700

    Added a new script for movement

---
+++ GDCUnity/Assets/thomas_flight.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class thomas_flight : MonoBehaviour
+{
+
+    public float speed;
+    public float yawSpeed;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.useGravity = false;
+    }
+
+    void FixedUpdate()
+    {
+        float moveHoriz = 0;
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            moveHoriz++;
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            moveHoriz--;
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+
+        }
+
+        Vector3 movement = new Vector3(moveHoriz, 0, 0);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.CompareTag("Object"))
+        {
+            collider.gameObject.SetActive(false);
+        }
+    }
+}
