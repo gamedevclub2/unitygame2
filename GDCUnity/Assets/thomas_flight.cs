a228d3b94f522143f682bac8abb07d876c4420a1
RJHS\BeattyTh <BeattyTh@Student.RegisJesuit.com>
2017-04-11 13:10:27 -0600

    Completely changed the flight script (not currently working)

--- GDCUnity/Assets/thomas_flight.cs
+++ GDCUnity/Assets/thomas_flight.cs
@@ -18,60 +18,26 @@ public class thomas_flight : MonoBehaviour
 
     void FixedUpdate()
     {
-        float x = rb.position.x;
-        float y = rb.position.y;
-        float z = rb.position.z;
 
-        if (Input.GetKey(KeyCode.W))
+        if(Input.GetKeyDown(KeyCode.W))
         {
-            z = z + 1;
+            rb.AddRelativeForce(Vector3.forward * speed);
         }
 
-        if (Input.GetKey(KeyCode.S))
+        if(Input.GetKeyDown(KeyCode.S))
         {
-            z = z + 1;
+            rb.AddRelativeForce(Vector3.back * speed);
         }
 
-        if (Input.GetKey(KeyCode.E))
+        if(Input.GetKeyDown(KeyCode.E))
         {
-            x = x + 1;
+            rb.AddRelativeForce(Vector3.right * speed);
         }
 
-        if (Input.GetKey(KeyCode.Q))
+        if(Input.GetKeyDown(KeyCode.Q))
         {
-            x = x + 1;
+            rb.AddRelativeForce(Vector3.left * speed);
         }
-
-        if(Input.mousePosition.y < winY - 30)
-        {
-            Vector3 angle = new Vector3(15, 0, 0);
-            Quaternion deltaRot = Quaternion.Euler(angle * Time.deltaTime);
-            rb.MoveRotation(rb.rotation * deltaRot);
-        }
-
-        if (Input.mousePosition.y > winY + 30)
-        {
-            Vector3 angle = new Vector3(15, 0, 0);
-            Quaternion deltaRot = Quaternion.Euler(-angle * Time.deltaTime);
-            rb.MoveRotation(rb.rotation * deltaRot);
-        }
-
-        if (Input.mousePosition.x < winX - 20)
-        {
-            Vector3 angle = new Vector3(0, 0, 15);
-            Quaternion deltaRot = Quaternion.Euler(-angle * Time.deltaTime);
-            rb.MoveRotation(rb.rotation * deltaRot);
-        }
-
-        if (Input.mousePosition.x > winX + 20)
-        {
-            Vector3 angle = new Vector3(0, 0, 15);
-            Quaternion deltaRot = Quaternion.Euler(angle * Time.deltaTime);
-            rb.MoveRotation(rb.rotation * deltaRot);
-        }
-
-        Vector3 movePos = new Vector3(x, y, z);
-        rb.MovePosition(movePos);
     }
 
     void OnTriggerEnter(Collider collider)
